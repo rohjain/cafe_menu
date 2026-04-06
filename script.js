be4e45b73f8ff4df93afdb75d93b35f3191c8db9
rohja <rohjain74@gmail.com>
2026-04-06 20:10:40 +0530

    k

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-function showMenu(id){
-    document.getElementById("categories").style.display = "none";
-    document.querySelectorAll(".menu-section").forEach(sec => {
-      sec.style.display = "none";
-    });
-    document.getElementById(id).style.display = "block";
-  }
-  
-  function goBack(){
-    document.getElementById("categories").style.display = "grid";
-    document.querySelectorAll(".menu-section").forEach(sec => {
-      sec.style.display = "none";
-    });
-  }
-  
+// Interactive functionality has been removed in favor of a full single-page menu layout.
